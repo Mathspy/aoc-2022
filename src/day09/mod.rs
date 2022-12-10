5c712c08d7bb8f13d07c879d39ad5d877714a8da
Mathspy <mathspy257@gmail.com>
2022-12-10 13:24:39 -0500

    Scaffold day 09

---
+++ src/day09/mod.rs
@@ -0,0 +1,9 @@
+use anyhow::Result;
+
+pub fn part1() -> Result<String> {
+    todo!()
+}
+
+pub fn part2() -> Result<String> {
+    todo!()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+mod day09;
+
+use anyhow::{bail, Result};
+
+fn main() -> Result<()> {
+    let mut args = std::env::args();
+    let _ = args.next();
+    let output = match (args.next().as_deref(), args.next().as_deref()) {
+        (Some("09"), Some("1")) => day09::part1()?,
+        (Some("09"), Some("2")) => day09::part2()?,
+        (arg1, arg2) => bail!("Invalid arguments {arg1:?} {arg2:?}"),
+    };
+
+    println!("{output}");
+
+    Ok(())
 }
